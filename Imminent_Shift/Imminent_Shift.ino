8dd316d0ed72a7fd813160f8e7a351b5c953aefd
Phillip D Stearns <phil@phillipstearns.com>
2015-05-22 14:48:14 -0400

    More Mod Control
    Added Internal Mod Rate Controls for Mod1 + Mod2

--- Imminent_Shift/Imminent_Shift.ino
+++ Imminent_Shift/Imminent_Shift.ino
@@ -12,15 +12,25 @@
 
 #define REG_L_OUT 2
 #define REG_R_OUT 3
+#define REG_L_AUX_OUT 4
+#define REG_R_AUX_OUT 5
+
 
 volatile boolean REG_STATES[REGISTER_SIZE]; //used as two independent registers, REG_L (0-31) and REG_R (32-63)
-volatile int count = 0;
+volatile unsigned long int count = 0;
 volatile boolean REG_L_IN;
 volatile boolean REG_R_IN;
+//volatile boolean L_DIV_TWO;
+//volatile boolean R_DIV_TWO;
+
+//volatile boolean LAST_REG_L_IN;
+//volatile boolean LAST_REG_R_IN;
 
 boolean update = true;
 
 int DISPLAY_REFRESH_RATE = 1;
+volatile long int MOD1_RATE = 1;
+volatile long int MOD2_RATE = 1;
 long int FREQUENCY = 8192;
 long int MULTIPLIER = 16;
 
@@ -28,9 +38,9 @@ byte BYTE_1 = 0;
 byte BYTE_2 = 0;
 
 int TAP_VALUES[4] = {15, 31, 0, 0}; //stores the tap position values 0-63, 0-31 = Left Register; 32-63 = Right Register;
-int TAP_EN[4] = {1, 2, 0, 0}; // uses 0 - 3 to designate which taps are enabled for each bit output to the left and right registers 0 or 0x00 = neither, 1 or 0x01 = Left, 2 or 0x10 = Right, 3 or 0x11 = both
-boolean MOD_STATES[4] = {0, 0, 0, 0};
-int MOD_EN[4] = {0, 0, 0, 0};
+int TAP_EN[4] = {1, 2, 0, 0}; // uses 0 - 3 to designate which taps are enabled for each bit output to the left and right registers 0 or 0x00 = neither, 1 or 0x01 = Left, 2 or 0x10 = Right, 3 or 0x11 = bothvoltatile
+volatile boolean MOD_STATES[4] = {0, 0, 0, 0};
+int MOD_EN[4] = {1, 2, 0, 0}; // uses 0 - 3 to designate which mod inputs are enabled for each bit output to the left and right registers 0 or 0x00 = neither, 1 or 0x01 = Left, 2 or 0x10 = Right, 3 or 0x11 = both
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -52,9 +62,16 @@ void setup() {
       REG_STATES[i] = false;
     }
   }
+  
+//  L_DIV_TWO = false;
+//  R_DIV_TWO = false;
+//  LAST_REG_L_IN = false;
+//  LAST_REG_R_IN = false;
 
   pinMode(REG_L_OUT, OUTPUT);
   pinMode(REG_R_OUT, OUTPUT);
+  pinMode(REG_L_AUX_OUT, OUTPUT);
+  pinMode(REG_R_AUX_OUT, OUTPUT);
   pinMode(OUTPUT_REG_CLK, OUTPUT);
   pinMode(OUTPUT_REG_DATA, OUTPUT);
   pinMode(OUTPUT_REG_STROBE, OUTPUT);
@@ -83,9 +100,8 @@ void serial() {
     //RLRLRLRL XXXXX011=tap enable values, RL,RL,RL,RL set whether this data is passed to L, R registers neither or both.
     //XXXXDDDD XXXXX100=modulation position values, DDDD directly sets the MOD state,
     //RLRLRLRL XXXXX101=modulation enable values, RL,RL,RL,RL set whether this data is passed to L, R registers neither or both.
-    //DDDDDDDD xxxxx000=refresh rate, DDDDDDDD sets the refreh rate (relative to clock frequency), 0 is stop, 1 = realtime, n > 1 = every n register clock cycles
-    //
-    //BYTE_1 of 11111111 allows direct setting of the register. The next eight bytes are loaded into BYTE_1 where the first byte received is = 0 - 7
+    //DDDDDDDD xxxxx110=refresh rate, DDDDDDDD sets the refreh rate (relative to clock frequency), 0 is stop, 1 = realtime, n > 1 = every n register clock cycles
+    //DDDDDDDD DDDDM111= Internal Mod rates where DDDDDDDD DDDD is the mod rate and M is the MOD ID 0 = MOD1_RATE, 1 = MOD2_RATE
 
     int ADDRESS = BYTE_1 & B00000111;
 
@@ -128,12 +144,16 @@ void serial() {
     } else if (ADDRESS == 6) {
       DISPLAY_REFRESH_RATE = BYTE_2;
     }
-      else if (ADDRESS == 7) {
-      REG_STATES[0] = 1;
+    else if (ADDRESS == 7) {
+      if((BYTE_1 & B00001000) >> 3 == 0){
+        MOD1_RATE = (BYTE_1 >> 4) | (BYTE_2 << 4);
+      } else if ((BYTE_1 & B00001000) >> 3 == 1){
+        MOD2_RATE = (BYTE_1 >> 4) | (BYTE_2 << 4);
+      }
     }
 
-    if ((FREQUENCY * MULTIPLIER) < 175) {
-      Timer1.setPeriod(175);
+    if ((FREQUENCY * MULTIPLIER) < 200) {
+      Timer1.setPeriod(200);
     } else {
       Timer1.setPeriod(FREQUENCY * MULTIPLIER);
     }
@@ -145,7 +165,7 @@ void displayRegisters() {
 
   for (int i = 0 ; i < REGISTER_SIZE / 2 ; i++) {
     PORTB = (REG_STATES[15 - i] ^ REG_STATES[16 + i]) << 3; //digitalWrite(OUTPUT_REG_DATA, ); 32 light version
-//    PORTB = (REG_STATES[31 - i] ^ REG_STATES[32 + i]) << 3; //digitalWrite(OUTPUT_REG_DATA, ); 64 light version
+    //    PORTB = (REG_STATES[31 - i] ^ REG_STATES[32 + i]) << 3; //digitalWrite(OUTPUT_REG_DATA, ); 64 light version
     PORTB |= B00000100; //digitalWrite(OUTPUT_REG_CLK, 1);
     PORTB = B00000000; //digitalWrite(OUTPUT_REG_CLK, 0);
   }
@@ -160,6 +180,14 @@ void callback() {
   REG_L_IN = 0;
   REG_R_IN = 0;
 
+  if (count % MOD1_RATE == 0) {
+    MOD_STATES[0] = !MOD_STATES[0];
+  }
+
+  if (count % MOD2_RATE == 0) {
+    MOD_STATES[1] = !MOD_STATES[1];
+  }
+
   for (int i = 0 ; i < 4 ; i ++) {
     REG_L_IN ^= (REG_STATES[TAP_VALUES[i]] & (TAP_EN[i] & B00000001 )) ^ (MOD_STATES[i] & (MOD_EN[i] & B00000001 ));
     REG_R_IN ^= (REG_STATES[TAP_VALUES[i]] & ((TAP_EN[i] & B00000010 ) >> 1)) ^ (MOD_STATES[i] & ((MOD_EN[i] & B00000010) >> 1));
@@ -171,13 +199,21 @@ void callback() {
 
   REG_STATES[0] = REG_L_IN;
   REG_STATES[16] = REG_R_IN; // 32 light version
-//  REG_STATES[32] = REG_R_IN; // 64 light version
+  //  REG_STATES[32] = REG_R_IN; // 64 light version
+  
+//  if(REG_L_IN == 1 && LAST_REG_L_IN == 0) L_DIV_TWO = !L_DIV_TWO;
+//  if(REG_R_IN == 1 && LAST_REG_R_IN == 0) R_DIV_TWO = !R_DIV_TWO;
 
-  PORTD = (REG_L_IN << 2) | (REG_R_IN << 3);
+  PORTD = (REG_L_IN << 2) | (REG_R_IN << 3) | REG_STATES[TAP_VALUES[0]] << 4 | REG_STATES[TAP_VALUES[1]] << 5 | REG_STATES[TAP_VALUES[2]] << 6 | REG_STATES[TAP_VALUES[3]] << 7;
 
   if (count % DISPLAY_REFRESH_RATE == 0) {
     displayRegisters();
   }
+  
+//  LAST_REG_L_IN = REG_L_IN;
+//  LAST_REG_R_IN = REG_R_IN;
+
+
 
   count++;
 }
